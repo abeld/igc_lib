4f62edb11ce48306bb798505093a17a8bf80c22e
Daniel Abel <daniel.abel@maven7.com>
2020-02-08 23:11:58 +0100

    add setup.py -- not tested yet

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='igc_lib',
+    version='0.1',
+
+    packages=['igc_lib',
+              ],
+    install_requires=[
+        'simplekml >= 1.2.2',
+    ],
+
+    author='Marcin Osowski',
+    author_email='??',
+    description='A simple library to parse IGC logs and extract thermals.',
+    url='https://github.com/xiadz/igc_lib.git',
+)
